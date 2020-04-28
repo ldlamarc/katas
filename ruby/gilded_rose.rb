dd5afe4378eaca26d45c14dc0ca9304be2ccd63a
ldlamarc <laurens.dlm@gmail.com>
2020-04-28 17:03:53 +0200

    Move non public facing methods to private

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -12,32 +12,6 @@ class GildedRose
 end
 
 class ItemUpdater < SimpleDelegator
-  def max_quality?
-    quality >= 50
-  end
-
-  def expired?
-    sell_in < 0
-  end
-
-  def hot?
-    hot_item? && sell_in < 10
-  end
-
-  def very_hot?
-    hot_item? && sell_in < 5
-  end
-
-  def quality_increment
-    increment = expired? ? 2 : 1
-    increment += 1 if hot?
-    increment += 1 if very_hot?
-    # Assumption that appreciating items also appreciate faster if conjured
-    # rationale: Brie also appreciates faster if expired
-    increment *= 2 if conjured?
-    increment
-  end
-
   def update_state
     return if legendary?
 
@@ -45,46 +19,74 @@ class ItemUpdater < SimpleDelegator
     update_quality
   end
 
-  def update_quality
-    if hot_item? && expired?
-      self.quality = 0
-      return
+  private
+
+    def max_quality?
+      quality >= 50
     end
 
-    if appreciating?
-      increase_quality
-    else
-      decrease_quality
+    def expired?
+      sell_in < 0
     end
-  end
 
-  def increase_quality
-    self.quality += quality_increment unless max_quality?
-  end
+    def hot?
+      hot_item? && sell_in < 10
+    end
 
-  def decrease_quality
-    self.quality -= quality_increment if quality.positive?
-  end
+    def very_hot?
+      hot_item? && sell_in < 5
+    end
 
-  def decrease_sell_in
-    self.sell_in -= 1
-  end
+    def quality_increment
+      increment = expired? ? 2 : 1
+      increment += 1 if hot?
+      increment += 1 if very_hot?
+      # Assumption that appreciating items also appreciate faster if conjured
+      # rationale: Brie also appreciates faster if expired
+      increment *= 2 if conjured?
+      increment
+    end
 
-  def conjured?
-    name.start_with?("Conjured")
-  end
+    def update_quality
+      if hot_item? && expired?
+        self.quality = 0
+        return
+      end
 
-  def appreciating?
-    ["Aged Brie"].include?(name) || hot_item?
-  end
+      if appreciating?
+        increase_quality
+      else
+        decrease_quality
+      end
+    end
 
-  def hot_item?
-    name.start_with?("Backstage")
-  end
+    def increase_quality
+      self.quality += quality_increment unless max_quality?
+    end
 
-  def legendary?
-    ["Sulfuras, Hand of Ragnaros"].include? name
-  end
+    def decrease_quality
+      self.quality -= quality_increment if quality.positive?
+    end
+
+    def decrease_sell_in
+      self.sell_in -= 1
+    end
+
+    def conjured?
+      name.start_with?("Conjured")
+    end
+
+    def appreciating?
+      ["Aged Brie"].include?(name) || hot_item?
+    end
+
+    def hot_item?
+      name.start_with?("Backstage")
+    end
+
+    def legendary?
+      ["Sulfuras, Hand of Ragnaros"].include? name
+    end
 end
 
 class Item
